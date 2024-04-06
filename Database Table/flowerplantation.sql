b61ebf58c807d2286652e3736330af1f8e38546b
Ehtixham Amjad <ehtixhamamjad@gmail.com>
2024-04-06 17:06:03 +0500

    database update 6-apr

--- Database Table/flowerplantation.sql
+++ Database Table/flowerplantation.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 02, 2024 at 02:19 PM
+-- Generation Time: Apr 06, 2024 at 01:22 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
@@ -34,6 +34,33 @@ CREATE TABLE `admin` (
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `email`, `password`) VALUES
+(1, 'admin', 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `favorite_flower`
+--
+
+CREATE TABLE `favorite_flower` (
+  `flower_id` int(12) NOT NULL,
+  `flower_image_name` varchar(250) NOT NULL,
+  `flower_name` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `favorite_flower`
+--
+
+INSERT INTO `favorite_flower` (`flower_id`, `flower_image_name`, `flower_name`) VALUES
+(31, 'f1.jpeg', 'rose'),
+(37, 'f2.jpeg', 'petal');
+
 -- --------------------------------------------------------
 
 --
@@ -42,6 +69,7 @@ CREATE TABLE `admin` (
 
 CREATE TABLE `flower` (
   `flower_id` int(11) NOT NULL,
+  `flower_nursery_id` int(12) NOT NULL,
   `flower_image_name` varchar(250) DEFAULT NULL,
   `flower_name` varchar(255) DEFAULT NULL,
   `flower_information` varchar(255) DEFAULT NULL,
@@ -65,14 +93,18 @@ CREATE TABLE `flower` (
 -- Dumping data for table `flower`
 --
 
-INSERT INTO `flower` (`flower_id`, `flower_image_name`, `flower_name`, `flower_information`, `color`, `season`, `category`, `altitude`, `height`, `area`, `grow_time`, `pesticide`, `fertilizer`, `disease`, `fragrance`, `shape`, `sunlight`, `watering`) VALUES
-(31, 'f1.jpeg', 'rose', 'info', 'red', 'Summer', 'outdoor', 1, 1, 'faisalabad', '2', 'pesticide', 'fertilizer', 'disease', 'fragrance', 'shape', 'sunlight', 'water'),
-(32, 'f1.jpeg', 'rose', 'info', 'red', 'Summer', 'outdoor', 1, 1, 'faisalabad', '2', 'pesticide', 'fertilizer', 'disease', 'fragrance', 'shape', 'sunlight', 'water'),
-(33, 'f2.jpeg', 'petal', 'info', 'yellow', 'Autumn', 'outdoor', 1, 1, 'lahore', '2', 'pesticide', 'fert', 'dis', 'frag', 'shape', 'sun', 'water'),
-(34, 'f2.jpeg', 'petal', 'info', 'yellow', 'Autumn', 'outdoor', 1, 1, 'lahore', '2', 'pesticide', 'fert', 'dis', 'frag', 'shape', 'sun', 'water'),
-(35, 'f2.jpeg', 'petal', 'info', 'yellow', 'Autumn', 'outdoor', 1, 1, 'lahore', '2', 'pesticide', 'fert', 'dis', 'frag', 'shape', 'sun', 'water'),
-(36, 'f2.jpeg', 'petal', 'info', 'yellow', 'Autumn', 'outdoor', 1, 1, 'lahore', '2', 'pesticide', 'fert', 'dis', 'frag', 'shape', 'sun', 'water'),
-(37, 'f2.jpeg', 'petal', 'info', 'yellow', 'Autumn', 'outdoor', 1, 1, 'islamabad', '2', 'pesticide', 'fert', 'dis', 'frag', 'shape', 'sun', 'water');
+INSERT INTO `flower` (`flower_id`, `flower_nursery_id`, `flower_image_name`, `flower_name`, `flower_information`, `color`, `season`, `category`, `altitude`, `height`, `area`, `grow_time`, `pesticide`, `fertilizer`, `disease`, `fragrance`, `shape`, `sunlight`, `watering`) VALUES
+(31, 0, 'f1.jpeg', 'rose', 'info', 'red', 'Summer', 'outdoor', 1, 1, 'faisalabad', '2', 'pesticide', 'fertilizer', 'disease', 'fragrance', 'shape', 'sunlight', 'water'),
+(33, 0, 'f2.jpeg', 'petal', 'info', 'yellow', 'Autumn', 'outdoor', 1, 1, 'lahore', '2', 'pesticide', 'fert', 'dis', 'frag', 'shape', 'sun', 'water'),
+(34, 0, 'f2.jpeg', 'petal', 'info', 'yellow', 'Autumn', 'outdoor', 1, 1, 'lahore', '2', 'pesticide', 'fert', 'dis', 'frag', 'shape', 'sun', 'water'),
+(35, 0, 'f2.jpeg', 'petal', 'info', 'yellow', 'Autumn', 'outdoor', 1, 1, 'lahore', '2', 'pesticide', 'fert', 'dis', 'frag', 'shape', 'sun', 'water'),
+(36, 0, 'f2.jpeg', 'petal', 'info', 'yellow', 'Autumn', 'outdoor', 1, 1, 'lahore', '2', 'pesticide', 'fert', 'dis', 'frag', 'shape', 'sun', 'water'),
+(37, 0, 'f2.jpeg', 'petal', 'info', 'yellow', 'Autumn', 'outdoor', 1, 1, 'islamabad', '2', 'pesticide', 'fert', 'dis', 'frag', 'shape', 'sun', 'water'),
+(38, 2, 'f2.jpeg', 'z', 'z', 'z', 'Winter', 'outdoor', 1, 1, 'z', 'z', 'z', 'z', 'z', 'z', 'z', 'z', 'z'),
+(39, 2, 'f2.jpeg', 'z', 'z', 'z', 'Winter', 'outdoor', 1, 1, 'z', 'z', 'z', 'z', 'z', 'z', 'z', 'z', 'z'),
+(40, 2, 'f2.jpeg', 'z', 'z', 'z', 'Winter', 'outdoor', 1, 1, 'z', 'z', 'z', 'z', 'z', 'z', 'z', 'z', 'z'),
+(41, 6, 'f1.jpeg', 'a', 'a', 'a', 'Autumn', 'outdoor', 1, 1, 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'),
+(45, 19, 'f1.jpeg', 'v', 'vd', 'd', 'Summer', 'outdoor', 3, 4, 'Karachi', 'w', 'd', 'w', 'd', 'd', 'd', 'd', 'd');
 
 -- --------------------------------------------------------
 
@@ -98,16 +130,8 @@ CREATE TABLE `flower_plan` (
 --
 
 INSERT INTO `flower_plan` (`plan_id`, `user_id`, `flower_name`, `season`, `flower_varieties`, `start_date`, `end_date`, `location`, `notes`, `budget_allocation`) VALUES
-(3, 1, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'Spring', 'ali ali ali', '2024-03-25', '2024-03-25', 'Peshawar', 'Ali\r\n', 3),
-(4, 1, 'z', 'z', 'z', '2024-03-25', '2024-03-25', 'z', 'z', 1),
-(5, 1, 'zain', 'Zain', 'Zain', '2024-03-25', '2024-03-25', 'Zain', 'Zain', 1),
 (6, 1, 'zain', 'Zain', 'Zain', '2024-03-25', '2024-03-25', 'Zain', 'Zain', 1),
-(7, 1, 'zain', 'Zain', 'Zain', '2024-03-25', '2024-03-25', 'Zain', 'Zain', 1),
-(8, 1, 'zain', 'Zain', 'Zain', '2024-03-25', '2024-03-25', 'Zain', 'Zain', 1),
-(9, 1, 'zain', 'Zain', 'Zain', '2024-03-25', '2024-03-25', 'Zain', 'Zain', 1),
-(16, 1, 'f', 'Spring', 'f', '2024-03-28', '2024-03-28', 'Karachi', 'f', 1),
-(17, 1, 'f', 'Spring', 'f', '2024-03-28', '2024-03-28', 'Karachi', 'f', 1),
-(18, 1, 'x', 'Summer', 'x', '2024-03-28', '2024-03-28', 'Peshawar', 'x', 1);
+(7, 1, 'zain', 'Zain', 'Zain', '2024-03-25', '2024-03-25', 'Zain', 'Zain', 1);
 
 -- --------------------------------------------------------
 
@@ -132,7 +156,9 @@ CREATE TABLE `nursery_owner` (
 --
 
 INSERT INTO `nursery_owner` (`id`, `name`, `address`, `city`, `zip`, `country`, `phone`, `email`, `password`) VALUES
-(1, 'Ehtisham Amjad', 'kuri road near faiza-bad and i-8', 'Islamabad', '44000', 'Pakistan', '03333705027', 'ali', '123');
+(1, 'Ehtisham Amjad', 'kuri road near faiza-bad and i-8', 'Islamabad', '44000', 'Pakistan', '03333705027', 'ali', '123'),
+(2, 'zain', 'zain', 'Multan', '123', '123', '123', 'zain@Gmailcom', '123'),
+(6, 'ali', 'ali', 'Lahore', 'ali', 'ali', '123', 'ali@Gmail.com', '123');
 
 -- --------------------------------------------------------
 
@@ -154,17 +180,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `phone`, `area`, `email`, `password`) VALUES
-(1, 'Ehtisham Amjad', '03333705027', 'Islamabad', 'ali@gmail.com', '123'),
-(2, 'zain', '1234', 'Lahore', 'zain', '123'),
-(3, 'zain', '1234', 'Lahore', 'zain', '123'),
-(4, 'zain', '123456789', 'Lahore', 'zain', '123'),
-(5, 'zain', '123456789', 'Lahore', 'zain', '123'),
-(6, 'ali', '123', 'Lahore', 'ali', '123'),
-(7, 'ali', '123', 'Lahore', 'ali', '123'),
-(8, 'ali', '123', 'Lahore', 'ali', '123'),
-(9, 'raza', '123', 'Lahore', 'raza', '123'),
-(10, 'toheed', '123', 'Islamabad', 'toheed@gmail.com', '123'),
-(11, 'talha', '123', 'Islamabad', 'talha@gmail.com', '123');
+(19, 'zain', '123', 'Karachi', 'zain@gmail.com', '123');
 
 --
 -- Indexes for dumped tables
@@ -208,13 +224,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `flower`
 --
 ALTER TABLE `flower`
-  MODIFY `flower_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `flower_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- AUTO_INCREMENT for table `flower_plan`
@@ -226,13 +242,13 @@ ALTER TABLE `flower_plan`
 -- AUTO_INCREMENT for table `nursery_owner`
 --
 ALTER TABLE `nursery_owner`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
