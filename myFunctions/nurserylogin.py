7891fd313829bf7f8996731ddf3c8458fd7546a8
Ehtixham Amjad <ehtixhamamjad@gmail.com>
2024-04-06 17:37:05 +0500

    flower detail name dynamic

--- myFunctions/nurserylogin.py
+++ myFunctions/nurserylogin.py
@@ -20,11 +20,23 @@ def addnurserylogin():
     query = "SELECT id FROM nursery_owner WHERE email = %s AND password = %s"
     cur.execute(query, (email, password))
     
+    # user_data = cur.fetchone()  # Fetch one row
+    
+    # # Consume remaining results
+    # cur.fetchall()
+    
+    # if user_data:  # If user exists (valid credentials)
+    #     session['id'] = user_data[0]  # Accessing the 'id' column value
+    #     cur.close()
+    #     conn.close()
+    #     return redirect('/nursery')
+    # else:  
+    #     cur.close()
+    #     conn.close()
+    #     return render_template("/nursery/login.html")
+    
     user_data = cur.fetchone()  # Fetch one row
-    
-    # Consume remaining results
-    cur.fetchall()
-    
+
     if user_data:  # If user exists (valid credentials)
         session['id'] = user_data[0]  # Accessing the 'id' column value
         cur.close()
@@ -33,4 +45,5 @@ def addnurserylogin():
     else:  
         cur.close()
         conn.close()
-        return render_template("/nursery/login.html")
+        return render_template("login.html")
+
